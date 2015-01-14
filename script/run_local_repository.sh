a8c5f5ccd486992a64727948af8537a9e6e37b3c
田中 和紀 <ktanaka@mixi.co.jp>
2015-01-14 19:38:18 +0900

    S3にimageをpushする仕組み作成

---
+++ script/run_local_repository.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# first setup
+# export AWS_ACCESS_KEY_ID=
+# export AWS_SECRET_ACCESS_KEY=
+# export DOCKER_REPOSITORY_BUCKET=
+
+docker run \
+         -e SETTINGS_FLAVOR=prod \
+         -e AWS_BUCKET=$DOCKER_REPOSITORY_BUCKET \
+         -e AWS_KEY=$AWS_ACCESS_KEY_ID \
+         -e AWS_SECRET=$AWS_SECRET_ACCESS_KEY \
+         -p 5000:5000 \
+         stackbrew/registry
